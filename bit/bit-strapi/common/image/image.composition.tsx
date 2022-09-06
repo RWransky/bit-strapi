65858e00c4232d7842dfad05e8db90c415d4983a
Rem Wransky <13735131+RWransky@users.noreply.github.com>
2022-09-05 22:54:58 -0400

    add high res image example

--- bit/bit-strapi/common/image/image.composition.tsx
+++ bit/bit-strapi/common/image/image.composition.tsx
@@ -3,17 +3,29 @@ import { StrapiImage } from './image';
 import imageJSON from './test/image.json';
 import gifJSON from './test/gif.json';
 import webpJSON from './test/webp.json';
+import highResJSON from './test/highres.json';
 
 const imgData = imageJSON.data.attributes;
 const gifData = gifJSON.data.attributes;
 const webpData = webpJSON.data.attributes;
+const highResData = highResJSON.data.attributes;
+
 
 export const BasicStrapiImage = (props) => (
   <div>
+    <StrapiImage attributes={highResData} />
     <StrapiImage attributes={imgData} />
     <StrapiImage attributes={gifData} />
     <StrapiImage attributes={webpData} />
   </div>
+);
+
+exprt const BasicStrapiJpegImage = (props) => {
+  <StrapiImage attributes={imgData} />
+}
+
+export const BasicStrapiWebpImage = (props) => (
+  <StrapiImage attributes={webpData} />
 );
 
 export const BasicStrapiGifImage = (props) => (
